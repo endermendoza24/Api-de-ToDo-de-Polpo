6767a4bc3c4a1a9bd163ddb96adf14f2222a388a
Endersson Mendoza <enderalonso24@gmail.com>
2021-02-11 18:54:50 -0600

    cambios en la conexion del servidor y la base de datos

--- src/rutas/tarea.rutas.js
+++ src/rutas/tarea.rutas.js
@@ -1,9 +1,28 @@
 express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res ) =>{
-    res.json({
-        status : 'API WORKS'
-    })
+const Task = require('../modelos/tareas.js');
+
+
+
+router.get('/', async(req, res ) =>{
+
+    const task = await Task.find();
+    console.log(task);
+    res.json('estupida y sensual programacion');
+});
+
+router.post('/', async(req, res) =>{
+    // console.log(req.body); //  el navegador envia datos y se reciben através de eeste metodo
+    const {title, descripcion} = req.body;
+    const task = new Task({title, descripcion}); //  esto crea una nueva tarea
+    console.log(task); 
+    await task.save(); //  estoa guarda los datos en la bd, pero de manera asíncrona
+    res.json({status:'task saved'});
 })
+
+
+
+
+
 module.exports = router;
